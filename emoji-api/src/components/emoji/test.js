07e5f552b51181132a66f5545ade84d4698cffe0
Response051 <Response051@gmail.com>
2023-02-27 19:33:12 +0100

    the functionalities have been updated

---
+++ emoji-api/src/components/emoji/test.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import emojiData from "./emoji.json";
+
+function Emoji() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const newData = emojiData.filter((emoji) =>
+      emoji.title.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+    setData(newData);
+  }, [search]);
+
+  return (
+    <div>
+      <h1>Emoji Search</h1>
+      <input
+        type="text"
+        placeholder="search"
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
+      {data.map((emoji) => (
+        <h2 key={emoji.title}>
+          {emoji.title} {emoji.symbol}
+        </h2>
+      ))}
+    </div>
+  );
+}
+export default Emoji;
